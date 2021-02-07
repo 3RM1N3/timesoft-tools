2ec4dcd990cb865dcf183f6b030e0ce19a129288
3RM1N3 <wangyu7439@gmail.com>
2021-02-07 11:15:55 +0800

    0.2版本

--- cli/鲜总分件/funcs.go
+++ cli/鲜总分件/funcs.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"path"
 	"strconv"
 	"strings"
 
@@ -10,18 +14,17 @@ import (
 )
 
 // 读取数据源表格
-func readSourcesExcel(filePath string) (map[string][]int, error) {
+func readSourcesExcel(filePath, sheetName string) (map[string][]int, error) {
 	log.Println("检查表格数据：", filePath)
 
 	f, err := excelize.OpenFile(filePath)
 	if err != nil {
-		log.Printf("表格文件打开失败：%s，第一个 Sheet\n", filePath)
+		log.Println("表格文件打开失败：", filePath)
 		return nil, err
 	}
-	sheetName := f.GetSheetList()[0]
 	rows, err := f.GetRows(sheetName)
 	if err != nil {
-		log.Printf("表格文件打开失败：%s，第一个 Sheet\n", filePath)
+		log.Printf("表格文件打开失败：%s，Sheet名称：%s\n", filePath, sheetName)
 		return nil, err
 	}
 	sourceMap := map[string][]int{}
@@ -30,14 +33,14 @@ func readSourcesExcel(filePath string) (map[string][]int, error) {
 		if len(row) < 2 {
 			log.Printf("表格数据有空值，第 1 个 Sheet，第 %d 行\n", i+1)
 			haveErrors = true
-			continue
+			break
 		}
 		someThing := strings.TrimSpace(row[0])
 		pageNum, err := strconv.Atoi(strings.TrimSpace(row[1]))
 		if err != nil {
 			log.Printf("表格数据 B 列有非整数值，第 1 个 Sheet，第 %d 行\n", i+1)
 			haveErrors = true
-			continue
+			break
 		}
 		arr, _ := sourceMap[someThing]
 		arr = append(arr, pageNum)
@@ -50,5 +53,65 @@ func readSourcesExcel(filePath string) (map[string][]int, error) {
 }
 
 func do(excelPath, sheetName, folderPath string) {
+	dataMap, err := readSourcesExcel(excelPath, sheetName) // 读取excel文件
+	if err != nil {
+		log.Println("表格文件打开失败：", err)
+		return
+	}
+	fileMap, err := getFileMap(folderPath) // 读取文件夹
+	if err != nil {
+		log.Println("文件夹读取失败：", folderPath)
+		return
+	}
+	for folderName, maxFileNum := range fileMap {
+		currentDir := path.Join(folderPath, folderName)
+		neededList, ok := dataMap[folderName]
+		if !ok {
+			continue
+		}
+		neededList = append(neededList, maxFileNum+1)
+		for i := 0; i < len(neededList)-1; i++ {
+			subFolder := fmt.Sprintf("%s-%04d", folderName, i+1)
+			moveToDir := path.Join(currentDir, subFolder)
+			os.MkdirAll(moveToDir, 0755)
+			for j := neededList[i]; j < neededList[i+1]; j++ {
+				fileName := fmt.Sprintf("%d.jpg", j)
+				log.Printf("将 %s 从 %s 移动到 %s\n", fileName, currentDir, moveToDir)
+				err := os.Rename(path.Join(currentDir, fileName), path.Join(moveToDir, fileName)) // 文件移动操作
+				if err != nil {
+					log.Println("移动失败：", err)
+				}
+			}
+		}
+	}
+	log.Println("完成")
+}
 
+func getFileMap(folderPath string) (map[string]int, error) {
+	log.Println("读取文件夹：", folderPath)
+	files, err := ioutil.ReadDir(folderPath)
+	if err != nil {
+		log.Println("读取文件夹失败：", err)
+		return nil, err
+	}
+	fileMap := map[string]int{}
+	for _, file := range files {
+		if file.IsDir() {
+			fullPath := path.Join(folderPath, file.Name())
+			log.Println("读取文件夹：", fullPath)
+			f, err := ioutil.ReadDir(fullPath)
+			if err != nil {
+				log.Println("读取文件夹错误：", fullPath)
+				continue
+			}
+			imgCounter := 0
+			for _, imgFile := range f {
+				if !imgFile.IsDir() && strings.HasSuffix(imgFile.Name(), ".jpg") {
+					imgCounter++
+				}
+			}
+			fileMap[file.Name()] = imgCounter
+		}
+	}
+	return fileMap, nil
 }
--- cli/鲜总分件/main.go
+++ cli/鲜总分件/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"strings"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -26,26 +26,47 @@ func init() {
 }
 
 func main() {
+	usage := `
+使用帮助：
+
+    本软件可根据表格内数据实现基本的分件功能。
+
+*注意事项：
+
+1. 在*.xlsx文件所选中的Sheet中，不应包含标题，
+   且档号应在第一列，页号应在第二列；
+2. 所有操作均在原地操作，请提前进行文件备份。
+
+
+Author: 3RM1N3@时源科技
+`
+	runable := true
 	sheetList := []string{} // sheet列表
 	selectedSheet := ""     // 选定的sheet页
 	myApp := app.New()
 	window := myApp.NewWindow("分件") // 新建窗口
+
+	statusBar := widget.NewLabel("选择表格文件、数据所在Sheet与项目文件夹后点击运行开始")
 
 	tablePathEntry := widget.NewEntry() // 用于显示所选择xlsx文件的文本框
 	folderEntry := widget.NewEntry()    // 用于显示所选文件夹的文本框
 	sheetSelect := new(widget.Select)
 	sheetSelect.OnChanged = func(s string) {
 		selectedSheet = s
-		log.Println(selectedSheet)
+		log.Println("选择Sheet：", selectedSheet)
+		statusBar.SetText("请选择项目文件夹")
 	}
 	sheetSelect.Disable()
+
+	usageLabel := widget.NewLabel(usage)
 
 	chooseTableButton := widget.NewButton("...", func() { // 选择表格文件的按钮
 		dialog.ShowFileOpen(func(uc fyne.URIReadCloser, e error) {
 			if e != nil || uc == nil {
-				log.Println("wrong1")
+				log.Println(e)
 				return
 			}
+			statusBar.SetText("请选择需要使用的Sheet页")
 			defer uc.Close()
 
 			f, err := excelize.OpenReader(uc)
@@ -56,7 +77,6 @@ func main() {
 			sheetList = f.GetSheetList()
 			filePath := uc.URI().String()
 			tablePathEntry.SetText(strings.TrimPrefix(filePath, "file://"))
-			fmt.Println(sheetList)
 			sheetSelect.Enable()
 			sheetSelect.Options = sheetList
 		}, window)
@@ -67,8 +87,40 @@ func main() {
 			if e != nil || lu == nil {
 				return
 			}
+			if tablePathEntry.Text == "" {
+				statusBar.SetText("请选择表格文件")
+			} else if selectedSheet == "" {
+				statusBar.SetText("请选择需要使用的Sheet页")
+			} else {
+				statusBar.SetText("单击运行以开始")
+			}
 			folderEntry.SetText(strings.TrimPrefix(lu.String(), "file://"))
 		}, window)
+	})
+
+	runButton := widget.NewButton("运行", func() { // 运行按钮
+		if tablePathEntry.Text == "" || folderEntry.Text == "" || selectedSheet == "" {
+			dialog.ShowInformation("错误", "尚有字段为空！", window)
+			return
+		}
+		cnf := dialog.NewConfirm("注意", "此操作执行后将不可逆！\n请提前进行文件备份并确认您已知悉程序运行后果！确定仍要继续吗？", func(r bool) {
+			if !r {
+				return
+			}
+			runable = false
+			statusBar.SetText("处理中...请勿进行其他操作")
+			do(tablePathEntry.Text, selectedSheet, folderEntry.Text) // 开始处理
+			tablePathEntry.SetText("")
+			folderEntry.SetText("")
+			selectedSheet = ""
+			sheetSelect.Options = []string{}
+			runable = true
+			statusBar.SetText("完毕")
+			dialog.ShowInformation("提示", "处理完毕", window)
+		}, window)
+		cnf.SetDismissText("取消")
+		cnf.SetConfirmText("确定")
+		cnf.Show()
 	})
 
 	ctnr := container.NewVBox( // 容器
@@ -97,18 +149,37 @@ func main() {
 			folderEntry,
 		),
 		widget.NewSeparator(),
-		container.NewMax(
-			widget.NewButton("运行", func() { // 运行按钮
-				if tablePathEntry.Text == "" || folderEntry.Text == "" || selectedSheet == "" {
-					dialog.ShowInformation("错误", "尚有字段为空！", window)
-					return
-				}
-				do(tablePathEntry.Text, selectedSheet, folderEntry.Text) // 开始处理
-			}),
-		),
+		runButton,
+		usageLabel,
 	)
+	go func() {
+		logFile, err := os.OpenFile("分件日志.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
+		if err != nil {
+			dialog.ShowInformation("提示", "日志文件打开失败，请检查文件是否被占用；\n接下来的操作会产生效果但不会在日志中体现。", window)
+			return
+		}
+		log.SetOutput(logFile) // 将文件设置为log输出的文件
+		//log.SetFlags(log.LstdFlags | log.Lshortfile | log.LUTC)
+	}()
+	go func() {
+		for {
+			time.Sleep(200 * time.Millisecond)
+			if !runable {
+				runButton.Disable()
+				continue
+			}
+			runButton.Enable()
+		}
+	}()
 
-	window.SetContent(ctnr)
-	window.Resize(fyne.NewSize(600, 400))
+	window.SetContent(
+		container.NewBorder(
+			nil,
+			statusBar,
+			nil,
+			nil,
+			ctnr,
+		))
+	window.Resize(fyne.NewSize(600, 500))
 	window.ShowAndRun()
 }
